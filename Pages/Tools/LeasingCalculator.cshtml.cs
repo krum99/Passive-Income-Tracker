554afb28e0abb3299ae037dd177e7daf7f236204
krum99 <79230210+krum99@users.noreply.github.com>
2025-05-09 15:43:38 +0300

    (feat) Adding Leasing calculator

---
+++ Pages/Tools/LeasingCalculator.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PassiveMoneyTracker.Pages.Tools
+{
+    public class LeasingCalculatorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
